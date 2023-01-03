f9af8b2d9bcbe937f1acebe2ab0e0934842e641d
S. M. Sujan <smsujon50@gmail.com>
2023-01-03 13:39:19 +0600

    React-bootstrap Installed, Component Structure and Layout Created

---
+++ src/Layout/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from '../Components/Home/Home/Home';
+import Footer from '../Components/Shared/Footer';
+import Header from '../Components/Shared/Header';
+
+const Main = () => {
+    return (
+        <>
+            <Header></Header>
+            <Home></Home>
+            <Footer></Footer>
+        </>
+    );
+};
+
+export default Main;
